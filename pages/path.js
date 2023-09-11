4a26616b4f686048dd2da06342c072cd792a1207
umairmaseed <umk.95@hotmail.com>
2023-09-11 16:44:27 +0500

    Path page created

--- pages/path.js
+++ pages/path.js
@@ -11,8 +11,18 @@ function path() {
       <main>
         <div className="treasure-container">
           <div className="treasure-img-box">
-            <div style={{ textAlign: 'center' }}>
+            <div className="treasure-images">
               <img src="/treasure.png" className="treasure-img" />
+              <img
+                src="user3.jpg"
+                className="user-treaser"
+                style={{ borderRadius: '50%' }}
+              />
+              <img src="/verified.gif" className="verified1" />
+              <img src="/verified.gif" className="verified2" />
+              <img src="/four.png" className="level4" />
+              <img src="/five.png" className="level5" />
+              <img src="/six.png" className="level6" />
             </div>
           </div>
           <div className="treasure-text-box">
@@ -20,14 +30,10 @@ function path() {
               Master Text Editing in Graphic Design
             </h2>
             <p className="treasure-description">
-              Lorem ipsum, or lipsum as it is sometimes known, is dummy text
-              used in laying out print, graphic or web designs. The passage is
-              attributed to an unknown Lorem ipsum, or lipsum as it is sometimes
-              known, is dummy text used in laying out print, graphic or web
-              designs. The passage is attributed to an unknown Lorem ipsum, or
-              lipsum as it is sometimes known, is dummy text used in laying out
-              print, graphic or web designs. The passage is attributed to an
-              unknown
+              Learn the art of text editing in Canva and unlock the power of
+              typography in your designs. Understand how fonts, sizes, and
+              styles impact visual communication, and gain the skills to create
+              compelling and professional text elements in your graphics.
             </p>
             <a className="main-nav-link nav-cta " href="/learning">
               Continue
